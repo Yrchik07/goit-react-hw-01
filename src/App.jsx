be6c929f3b77038a07b5432d2a5ef120c724db64
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2024-03-06 16:41:48 +0200

    filling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          alt="User avatar"
+        />
+        <p>Petra Marica</p>
+        <p>@pmarica</p>
+        <p>Salvador, Brasil</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>1000</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>2000</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>3000</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
