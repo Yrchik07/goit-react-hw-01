43370092cbfb156b13d83c47a809cabaf41249f3
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2024-03-10 16:51:24 +0200

    preliminary version

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import './App.css';
+import Profile from './components/Profile/Profile';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import { userData } from './userData.json';
+import { friends } from './friends.json';
+import { transactions } from './transactions.json';
 
 function App() {
   return (
-    <div>
-      <div>
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-        />
-        <p>Petra Marica</p>
-        <p>@pmarica</p>
-        <p>Salvador, Brasil</p>
-      </div>
-
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>1000</span>
-        </li>
-        <li>
-          <span>Views</span>
-          <span>2000</span>
-        </li>
-        <li>
-          <span>Likes</span>
-          <span>3000</span>
-        </li>
-      </ul>
-    </div>
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 }
 
